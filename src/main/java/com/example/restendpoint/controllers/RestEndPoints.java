cb10a223f5ecb107657e2032e8d178b168b3985f
¨ProjectD20 <projectdev0220@gmail.com>
2021-04-24 15:05:41 +0100

    initial commit

---
+++ src/main/java/com/example/restendpoint/controllers/RestEndPoints.java
@@ -0,0 +1,11 @@
+package com.example.restendpoint.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestEndPoints {
+	public RestEndPoints() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
