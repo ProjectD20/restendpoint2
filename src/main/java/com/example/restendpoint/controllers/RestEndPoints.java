c0cea614044ef5101ad9eeeb59e93cd46baa5771
¨ProjectD20 <projectdev0220@gmail.com>
2021-04-24 15:10:38 +0100

    first test

--- src/main/java/com/example/restendpoint/controllers/RestEndPoints.java
+++ src/main/java/com/example/restendpoint/controllers/RestEndPoints.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RestEndPoints {
 	public RestEndPoints() {
-		// TODO Auto-generated constructor stub
+		System.out.println("hello");
 	}
 
 }
